2b8b5d7eb08f6b610244142211d226ff80963397
Whippet Dev <whippet.dev@gmail.com>
2026-07-31 11:38:13 +0100

    Remove generated files from repository

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var hierarchySelector91C06B93F80548E6A8D87B1480DF361C: IVisualPlugin;
-export default hierarchySelector91C06B93F80548E6A8D87B1480DF361C;
