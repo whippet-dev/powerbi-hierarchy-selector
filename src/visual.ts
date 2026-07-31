e9a2fda91699df268a4c265ed342b426c6dfe51b
Whippet Dev <whippet.dev@gmail.com>
2026-07-31 10:54:46 +0100

    Added test data

--- src/visual.ts
+++ src/visual.ts
@@ -1,90 +1,51 @@
-/*
-*  Power BI Visual CLI
-*
-*  Copyright (c) Microsoft Corporation
-*  All rights reserved.
-*  MIT License
-*
-*  Permission is hereby granted, free of charge, to any person obtaining a copy
-*  of this software and associated documentation files (the ""Software""), to deal
-*  in the Software without restriction, including without limitation the rights
-*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-*  copies of the Software, and to permit persons to whom the Software is
-*  furnished to do so, subject to the following conditions:
-*
-*  The above copyright notice and this permission notice shall be included in
-*  all copies or substantial portions of the Software.
-*
-*  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-*  THE SOFTWARE.
-*/
 "use strict";
 
 import powerbi from "powerbi-visuals-api";
-import { FormattingSettingsService } from "powerbi-visuals-utils-formattingmodel";
-import "./../style/visual.less";
+import "../style/visual.less";
 
 import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
 import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
 import IVisual = powerbi.extensibility.visual.IVisual;
-import IVisualEventService = powerbi.extensibility.IVisualEventService;
-
-import { VisualFormattingSettingsModel } from "./settings";
 
 export class Visual implements IVisual {
-    private events: IVisualEventService;
-    private target: HTMLElement;
-    private updateCount: number;
-    private textNode: Text;
-    private formattingSettings: VisualFormattingSettingsModel;
-    private formattingSettingsService: FormattingSettingsService;
+    private readonly container: HTMLDivElement;
 
-    constructor(options: VisualConstructorOptions) {
-        console.log('Visual constructor', options);
-        this.events = options.host.eventService;
-        this.formattingSettingsService = new FormattingSettingsService();
-        this.target = options.element;
-        this.updateCount = 0;
-        if (document) {
-            const new_p: HTMLElement = document.createElement("p");
-            new_p.appendChild(document.createTextNode("Update count:"));
-            const new_em: HTMLElement = document.createElement("em");
-            this.textNode = document.createTextNode(this.updateCount.toString());
-            new_em.appendChild(this.textNode);
-            new_p.appendChild(new_em);
-            this.target.appendChild(new_p);
-        }
+    public constructor(options: VisualConstructorOptions) {
+        this.container = document.createElement("div");
+        this.container.className = "hierarchy-selector";
+        options.element.appendChild(this.container);
     }
 
-    public update(options: VisualUpdateOptions) {
-        this.events.renderingStarted(options);
+    public update(options: VisualUpdateOptions): void {
+        this.container.innerHTML = "";
 
-        try {
-            this.formattingSettings = this.formattingSettingsService.populateFormattingSettingsModel(VisualFormattingSettingsModel, options.dataViews[0]);
+        const hierarchyLevels = [
+            "Continent",
+            "Country",
+            "Region",
+            "City",
+            "Site"
+        ];
 
-            console.log('Visual update', options);
-            if (this.textNode) {
-                this.textNode.textContent = (this.updateCount++).toString();
-            }
+        for (const levelName of hierarchyLevels) {
+            const level = document.createElement("div");
+            level.className = "hierarchy-level";
 
-            this.events.renderingFinished(options);
+            const label = document.createElement("div");
+            label.className = "hierarchy-level__label";
+            label.textContent = levelName;
+
+            const button = document.createElement("button");
+            button.className = "hierarchy-level__button";
+            button.type = "button";
+            button.textContent = "Select";
+
+            level.appendChild(label);
+            level.appendChild(button);
+            this.container.appendChild(level);
         }
-        catch (error) {
-            console.log('Error in update method', error);
-            this.events.renderingFailed(options, String(error))
-        }
-    }
 
-    /**
-     * Returns properties pane formatting model content hierarchies, properties and latest formatting values, Then populate properties pane.
-     * This method is called once every time we open properties pane or when the user edit any format property. 
-     */
-    public getFormattingModel(): powerbi.visuals.FormattingModel {
-        return this.formattingSettingsService.buildFormattingModel(this.formattingSettings);
+        this.container.style.width = `${options.viewport.width}px`;
+        this.container.style.height = `${options.viewport.height}px`;
     }
 }
